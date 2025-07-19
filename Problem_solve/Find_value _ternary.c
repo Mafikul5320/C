24c949b3b505eac83f0b782108eb1afcefe98e50
Mafikul Islam <mdmafikulislam5320@gmail.com>
2025-07-19 07:47:27 +0600

    Problem-3

---
+++ Problem_solve/Find_value _ternary.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, max;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    max = (a > b) ? a : b;
+
+    printf("The larger number is: %d", max);
+    return 0;
+}
